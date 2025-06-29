69ee2b60e91ca05053bb8c02d20255a4ce1d8248
kapil garg <kapilgarg236@gmail.com>
2025-06-29 08:44:13 +0530

    .

--- frontend/src/LoginSignup/LoginSignup.jsx
+++ frontend/src/LoginSignup/LoginSignup.jsx
@@ -23,6 +23,7 @@ export const LoginSignup = () => {
   };
 
   const handleSubmit = async () => {
+    console.log("BASE_URL =>", BASE_URL);
     try {
       if (action === "Login") {
         const res = await axios.post(`${BASE_URL}/auth/login`, {
