24bae1a9f94d964cea49ddf6cdebeb3b21127931
kapil garg <kapilgarg236@gmail.com>
2025-06-30 19:06:17 +0530

    changes for deployement

--- backend/src/main/java/com.expenseTracker/config/CorsConfig.java
+++ backend/src/main/java/com.expenseTracker/config/CorsConfig.java
@@ -1,15 +1,19 @@
 package com.expenseTracker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- backend/src/test/java/com/expenseTracker/ExpenseTrackerApplicationTests.java
+++ backend/src/test/java/com/expenseTracker/ExpenseTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.kapil.ExpenseTracker.ExpenseTracker;
+package com.expenseTracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
